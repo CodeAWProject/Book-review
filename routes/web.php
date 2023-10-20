f68c91bc5961d5976e79c3fa287821791c8acfd7
CodeAWProject <activityware@gmail.com>
2023-10-20 14:51:52 +0200

    Disabling Unused Actions in BookController

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@ Route::get('/', function () {
     return redirect()->route('books.index');
 });
 
-Route::resource('books', BookController::class);
+Route::resource('books', BookController::class)
+    ->only('index', 'show');
