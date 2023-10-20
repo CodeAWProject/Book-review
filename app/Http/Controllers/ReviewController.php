ec9b6e73d5f7dd138db0ad7b65cd6f82b9a67d81
CodeAWProject <activityware@gmail.com>
2023-10-20 15:34:41 +0200

    Implementing the logic to create a new book review and display it on the view

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Book;
 
 class ReviewController extends Controller
 {
@@ -18,17 +19,28 @@ class ReviewController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+
+     //It tells you which book the review was made for.
+    public function create(Book $book)
     {
-        //
+        return view('books.reviews.create', ['book' => $book]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+
+     //Automatically associated with this specific book
+    public function store(Request $request, Book $book)
     {
-        //
+        $data = $request->validate([
+            'review' => 'required|min:15',
+            'rating' => 'required|min:1|max:5|integer'
+        ]);
+
+        $book->reviews()->create($data);
+
+        return redirect()->route('books.show', $book);
     }
 
     /**
